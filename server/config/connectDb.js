546eae28cb05943597e5b7d2ddfda6c956e6c1b9
emmeravivar <em.meravivar@gmail.com>
2024-05-14 16:57:39 +0200

    without cors

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 //Conectar nuestra BBDD
 export const connectBD = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const connection = await mongoose.connect(
+      process.env.MONGO_URI
+      //   , {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // }
+    );
     const url = `${connection.connection.host}:${connection.connection.port}`;
     console.log(`MongoDB conecta en: ${url}`);
   } catch (error) {
