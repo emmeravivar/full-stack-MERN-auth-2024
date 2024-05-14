eb48873226474bf397350b7e2b06afc9a2c4ca9a
emmeravivar <em.meravivar@gmail.com>
2024-05-14 18:41:35 +0200

    run

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //Configuración del servidor
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
+// import cors from 'cors';
 import { connectBD } from './config/connectDb.js';
 import usersRoutes from './routes/User.routes.js';
 
@@ -20,21 +20,21 @@ connectBD();
 
 //Configurar CORS
 // WhiteList
-const whitelist = [
-  process.env.FRONT_END_URL,
-  process.env.FRONT_END_URL_LOCALHOST,
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    console.log(origin);
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-app.use(cors(corsOptions));
+// const whitelist = [
+//   process.env.FRONT_END_URL,
+//   process.env.FRONT_END_URL_LOCALHOST,
+// ];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     console.log(origin);
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
 
 //Routing
 app.use('/api/user', usersRoutes);
